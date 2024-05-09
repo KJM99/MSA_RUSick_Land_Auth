5aa3c781caa3b44992a0b6646eae48b935e64833
HuiSeokJeong <tobysug123@gmail.com>
2024-05-09 19:16:44 +0900

    feat: login메서드 기본 틀 구현

--- src/main/java/com/example/auth/service/AuthServiceImpl.java
+++ src/main/java/com/example/auth/service/AuthServiceImpl.java
@@ -1,4 +1,17 @@
 package com.example.auth.service;
 
+import com.example.auth.domain.entity.UserRepository;
+import com.example.auth.domain.response.LoginResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class AuthServiceImpl implements AuthService {
+    private UserRepository userRepository;
+
+    @Override
+    public LoginResponse login(String token) {
+        return null;
+    }
 }
