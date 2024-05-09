8661e624089447cce40a8d85c5ee68be0f1ec566
HuiSeokJeong <tobysug123@gmail.com>
2024-05-09 19:22:09 +0900

    feat: UserResponse 생성

---
+++ src/main/java/com/example/auth/domain/response/UserResponse.java
@@ -0,0 +1,11 @@
+package com.example.auth.domain.response;
+
+import java.time.LocalDate;
+
+public record UserResponse(
+        String id,
+        String email,
+        String nickname,
+        LocalDate birthDay,
+        String gender) {
+}
