06048bea05c73dd485e38c724b60b40b7cd1bc40
HuiSeokJeong <tobysug123@gmail.com>
2024-05-10 17:17:19 +0900

    refactor: 오타수정

--- src/main/java/com/example/auth/global/utils/JwtUtil.java
+++ src/main/java/com/example/auth/global/utils/JwtUtil.java
@@ -20,7 +20,7 @@ public class JwtUtil {
         String token = Jwts.builder()
                 .claim("id", user.getId())
                 .claim("nickname", user.getNickname())
-                .claim("birthday", user.getBirthDay())
+                .claim("birthDay", user.getBirthDay())
                 .expiration(new Date(System.currentTimeMillis() + expiration))
                 .signWith(secretKey)
                 .compact();
